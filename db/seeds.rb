e1ec466ef0e5ab244a27cb3715dc5b6bac3348a0
Julien Chim <julien.chim@gmail.com>
2018-05-16 13:02:14 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
-Restaurant.new(name: "McDonalds", category: "american", address: "75001 Paris")
-Restaurant.new(name: "pasta milano", category: "italian", address: "London")
-Restaurant.new(name: "sukiyaki", category: "japanese", address: "Tokyo")
-Restaurant.new(name: "robi", category: "italian", address: "New York")
-Restaurant.new(name: "thairesto", category: "thai", address: "75018 Paris")
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Generating seeds 5 restaurants'
+
+Restaurant.create(name: "McDonalds", category: "chinese", address: "75001 Paris")
+Restaurant.create(name: "pasta milano", category: "italian", address: "London")
+Restaurant.create(name: "sukiyaki", category: "japanese", address: "Tokyo")
+Restaurant.create(name: "robi", category: "japanese", address: "New York")
+Restaurant.create(name: "thairesto", category: "french", address: "75018 Paris")
